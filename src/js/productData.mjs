2a5d37ed6e8a104a411290fbbffdfe4fb8451ac0
Jesus Bejar <israelbejarii@gmail.com>
2023-05-03 09:42:25 -0600

    added comments to productData.mjs

--- src/js/productData.mjs
+++ src/js/productData.mjs
@@ -1,3 +1,6 @@
+// small module
+// convertToJson is not exported meaning we won't
+// run it outside of this module
 function convertToJson(res) {
   if (res.ok) {
     return res.json();
@@ -6,12 +9,17 @@ function convertToJson(res) {
   }
 }
 
+// the category defaults to tents 
+// so if we don't pass anything in
+// it will return tents
 export function getData(category = "tents") {
   return fetch(`../json/${category}.json`)
     .then(convertToJson)
     .then((data) => data);
 }
 
+// findProductByID is easier to understand than getData
+// async/wait functions are easier to write than .then() promises
 export async function findProductById(id) {
   const products = await getData();
   return products.find((item) => item.Id === id);
