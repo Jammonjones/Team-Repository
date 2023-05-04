2e943436f2d5cf0d1e7b7c19974e85d4714c61e6
Jesus Bejar <israelbejarii@gmail.com>
2023-05-04 10:28:47 -0600

    made the productdetails.mjs file

---
+++ src/js/productdetails.mjs
@@ -0,0 +1,6 @@
+// The productDetails.mjs module will be responsible for all of the functionality 
+// needed to lookup data for a specific product and display it in HTML. Good modules 
+// often follow a "black box" philosophy. The module should be very upfront about 
+// what it needs to function, but very opaque about the details of how it accomplishes 
+// it's work. This means in part that we should only expose as much as we need to for 
+// it to work.
