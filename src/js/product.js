0c717de04f69f3299b970e5e4d4c613452a77273
Jesus Bejar <israelbejarii@gmail.com>
2023-05-05 10:06:15 -0600

    eliminated fluff on product.js

--- src/js/product.js
+++ src/js/product.js
@@ -8,39 +8,3 @@ import productDetails from "./productDetails.mjs";
 // this should test findProductbyId and getParam functions
 const productId = getParam("product");
 productDetails(productId);
-
-function convertToJson(res) {
-  if (res.ok) {
-    return res.json();
-  } else {
-    throw new Error("Bad Response");
-  }
-}
-
-// get products data and add event listener
-async function init() {
-  products = await fetch("../json/tents.json").then(convertToJson);
-  document.getElementById("addToCart").addEventListener("click", addToCart);
-  console.log(products);
-}
-
-// add to cart button event handler
-function addToCart(e) {
-  const productId = e.target.dataset.id;
-  const product = products.find((item) => item.Id === productId);
-  console.log(typeof cartItems);
-  if (!Array.isArray(cartItems)) {
-    cartItems = [];
-  }
-  cartItems.push(product); // add the new product to cartItems array
-  setLocalStorage("so-cart", cartItems); // save cart items to local storage
-  console.log("Added to cart:", product);
-}
-
-// retrieve data from localstorage.
-function getLocalStorage(key) {
-  return JSON.parse(localStorage.getItem(key));
-}
-
-// call init function to fetch data and add event listener
-init();
