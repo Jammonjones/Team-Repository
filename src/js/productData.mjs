433c87b858758b4e3e02c6056287234164839c0a
KyerraBright <Kyerra@comcast.net>
2023-05-23 09:47:07 -0600

    team5 done

--- src/js/productData.mjs
+++ src/js/productData.mjs
@@ -1,12 +1,18 @@
 // small module
 // convertToJson is not exported meaning we won't
 // run it outside of this module
+const baseURL = import.meta.env.VITE_SERVER_URL
 function convertToJson(res) {
   if (res.ok) {
     return res.json();
   } else {
     throw new Error("Bad Response");
   }
+}
+export async function getData(category) {
+  const response = await fetch(baseURL + `products/search/${category}`);
+  const data = await convertToJson(response);
+  return data.Result;
 }
 
 // the category defaults to tents 
