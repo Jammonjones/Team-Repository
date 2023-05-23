9447aa1586acaa97ff5c3ea19b2fc08abf10abef
Jesus Bejar <israelbejarii@gmail.com>
2023-05-23 15:23:53 -0600

    populated product-list.js with main.js code

--- src/js/product-list.js
+++ src/js/product-list.js
@@ -1,2 +1,7 @@
 // that is responsible to render our list of products to the new product-list.js file??
 // productList(".product-list", "tents");
+import { productList } from "./productList.mjs";
+import { loadHeaderFooter } from "./utils.mjs";
+
+loadHeaderFooter();
+productList(".product-list", "tents");
