8793a2b37b57754a6a43897736a5dc4bf660d1ca
Jesus Bejar <israelbejarii@gmail.com>
2023-05-25 15:11:36 -0600

    removed setLocalStorage function

--- src/js/product.js
+++ src/js/product.js
@@ -1,8 +1,5 @@
-import { setLocalStorage, getParam } from "./utils.mjs";
+import { getParam } from "./utils.mjs";
 import productDetails from "./productDetails.mjs";
 
-
-// copied from example code
-// this should test findProductbyId and getParam functions
 const productId = getParam("product");
 productDetails(productId);
